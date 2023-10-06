8d6aa2889941d9db8379e4bafdddd9c9c41eecd4
Hannah <chia.hannahk@gmail.com>
2023-10-07 01:28:06 +0700

    Protect Routes
    Note that when going from home page to another route which you don't have access to, you might see a flicker...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProblemPage from './pages/problems/ProblemPage';
-import QuestionsPage from './pages/QuestionsPage';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import { useMediaQuery } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { hannahTheme, hannahThemeDark } from './constants/themes';
 import { ThemeContext } from './contexts/theme-context';
+import { Session } from '@supabase/supabase-js';
 import { Auth } from '@supabase/auth-ui-react';
-import AuthPage from './pages/AuthPage';
 import { supabase } from './main';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { UserPageMain, UserProfilesPage } from './pages/UserPage';
+// Components (for routes)
 import MainNavigationBar from './components/Navbar/MainNavigationBar';
+import NotFoundPage from './pages/NotFoundPage';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import ProblemPage from './pages/problems/ProblemPage';
+import QuestionsPage from './pages/QuestionsPage';
 
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const [theme, setTheme] = useState(prefersDarkMode ? 'dark' : 'light');
   const navigate = useNavigate();
   const queryClient = new QueryClient();
+  const [session, setSession] = useState<Session | null>();
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       async (event, session) => { // waits for the session to load
         console.log(event, session);
-        if (event == 'SIGNED_OUT') { // immediate redirection
+        setSession(session);
+        if (event == 'SIGNED_OUT') { // immediate redirection on signout
             navigate('/auth');
-          }
-        else if (!session) {
-          navigate('/auth');
         }
       }
     );
@@ -46,15 +48,24 @@ function App() {
             theme={theme == 'light' ? hannahTheme : hannahThemeDark}
           >
             <MainNavigationBar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/auth" element={<AuthPage />} />
-              <Route path="/user" element={<UserPageMain />} />
-              <Route path="/user/:id" element={<UserProfilesPage />} />
-              <Route path="/questions" element={<QuestionsPage />} />
-              {/* TODO: Change this to dynamic routing */}
-              <Route path="/problems" element={<ProblemPage />} />
-            </Routes>
+            {!session && (
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/auth" element={<AuthPage />} />
+                <Route path="*" element={<Navigate to='/auth' replace />} />
+              </Routes>
+            )}
+            {session && (
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/user" element={<UserPageMain />} />
+                <Route path="/user/:id" element={<UserProfilesPage />} />
+                <Route path="/questions" element={<QuestionsPage />} />
+                {/* TODO: Change this to dynamic routing */}
+                <Route path="/problems" element={<ProblemPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            )}
           </ThemeProvider>
         </ThemeContext.Provider>
       </Auth.UserContextProvider>
