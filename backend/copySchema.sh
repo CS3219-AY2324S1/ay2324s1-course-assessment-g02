58d7809f730587567d6239c4c3734b1bdbfca167
Neo Jing Xuan <neojxuan@gmail.com>
2023-10-10 01:33:54 +0800

    fix: add script to copy schema to child folders

---
+++ backend/copySchema.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the path to the parent folder
+parent_folder="$(dirname "$(readlink -f "$0")")"
+
+# Define the path to the schema file you want to copy
+schema_file="$parent_folder/prisma/schema.prisma"
+
+# Define the paths to the two child folders within the parent folder
+child_folder1="$parent_folder/questions/prisma"
+child_folder2="$parent_folder/users/prisma"
+
+# Copy the schema file to child folders
+cp "$schema_file" "$child_folder1/"
+cp "$schema_file" "$child_folder2/"
+
+echo "Schema file copied to child folders."
