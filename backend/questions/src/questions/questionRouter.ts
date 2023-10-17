332ed5a9dc218f698634dae54043bb9e7759fdb7
Neo Jing Xuan <neojxuan@gmail.com>
2023-10-17 22:27:14 +0800

    feat: question rating and attempts endpoints

--- backend/questions/src/questions/questionRouter.ts
+++ backend/questions/src/questions/questionRouter.ts
@@ -13,6 +13,8 @@ interface QuestionUpdateData {
   categories?: {
     set: { id: number }[]; // Array of category IDs for the 'set' operation
   };
+  avgRating?: number;
+  ratings?: number;
 }
 
 // Get all questions
@@ -79,7 +81,7 @@ questionRouter.post(
 questionRouter.put(
   '/:id',
   asyncHandler(async (req: Request, res: Response) => {
-    const { title, body, categories, complexity } = req.body;
+    const { title, body, categories, complexity, ratings } = req.body;
     const questionId = Number(req.params.id);
 
     const existingQuestion = await prisma.question.findUnique({
@@ -98,7 +100,12 @@ questionRouter.put(
     const updateData: QuestionUpdateData = {
       title: title || existingQuestion.title,
       body: body || existingQuestion.body,
-      complexity: complexity || existingQuestion.complexity
+      complexity: complexity || existingQuestion.complexity,
+      avgRating: ratings
+        ? (existingQuestion.ratings * existingQuestion.avgRating + ratings) /
+          (existingQuestion.ratings + 1)
+        : existingQuestion.avgRating,
+      ratings: ratings ? existingQuestion.ratings + 1 : existingQuestion.ratings
     };
 
     if (categories && categories.length > 0) {
@@ -138,3 +145,81 @@ questionRouter.delete(
     res.status(200).json({ message: 'Question deleted successfully' });
   })
 );
+
+// This route should be above the /attempts/:id route to avoid conflict
+// Get all question attempts for a user
+questionRouter.get(
+  '/attempts/users/:userId',
+  asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const user = await prisma.user.findUnique({
+      where: { userId: userId },
+      include: {
+        attemptedQuestion1: true,
+        attemptedQuestion2: true,
+      },
+    });
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+    const combinedAttempts = [
+      ...user.attemptedQuestion1,
+      ...user.attemptedQuestion2,
+    ];
+
+    res.status(200).json(combinedAttempts);
+  })
+);
+
+// Add question attempt
+questionRouter.post(
+  '/attempts',
+  asyncHandler(async (req: Request, res: Response) => {
+    const { questionId, userId1, userId2, code } = req.body;
+
+    const newAttempt = await prisma.attemptedQuestion.create({
+      data: {
+        questionId,
+        userId1,
+        userId2,
+        code
+      }
+    });
+
+    // Respond with a success message
+    res.status(201).json({
+      message: 'Question attempt created successfully',
+      questionAttempt: newAttempt
+    });
+  })
+);
+
+// Complete question attempt
+questionRouter.put(
+  '/attempts/:id',
+  asyncHandler(async (req: Request, res: Response) => {
+    const questionAttempt = await prisma.attemptedQuestion.findUnique({
+      where: { id : Number(req.params.id) }
+    });
+    await prisma.attemptedQuestion.update({
+      where: { id : Number(req.params.id) },
+      data: { completedAt: new Date() }
+    });
+
+    res.status(200).json({
+      message: 'Question attempt marked completed successfully',
+      questionAttempt: questionAttempt
+    });
+  })
+);
+
+// Get all question attempts
+questionRouter.get(
+  '/attempts',
+  asyncHandler(async (req: Request, res: Response) => {
+    const questionAttempts = await prisma.attemptedQuestion.findMany({});
+    res.status(200).json(questionAttempts);
+  })
+);
+
